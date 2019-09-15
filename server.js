a943ea22fdaf2f2658719a3861ad10e667409c97
Evan Goldstein <evan@icarusnet.me>
2019-09-15 12:22:52 -0400

    Dependencies

--- server.js
+++ server.js
@@ -5,6 +5,29 @@
 const express = require('express');
 const app = express();
 
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ posts: [], user: {}, count: 0 })
+    .write();
+
+// Add a post
+db.get('posts')
+    .push({ id: 1, title: 'lowdb is awesome'})
+    .write();
+
+// Set a user using Lodash shorthand syntax
+db.set('user.name', 'typicode')
+    .write();
+
+// Increment count
+db.update('count', n => n + 1)
+    .write();
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
